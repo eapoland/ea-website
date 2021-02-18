3545116fa13b87d76d2ea3911e9f0cc9ea61f0e1
Michał Hawełka <m.hawelka@gmail.com>
2021-02-18 20:09:07 +0100

    About page intro creation

--- src/pages/About/AboutPage.js
+++ src/pages/About/AboutPage.js
@@ -4,17 +4,31 @@ import Paragraph from "../../components/ContentSection/Paragraph";
 import SectionTitle from "../../components/ContentSection/SectionTitle";
 import { Row, Col, Card, CardTitle, CardText } from "reactstrap";
 import { useTranslation } from "react-i18next";
+import ScrollToTop from "../../components/ScrollToTop";
+import SectionTitleWithTranslation from "../../components/SectionTitle/SectionTitleWithTranslation";
 import "./AboutPage.scss";
 
 const AboutPage = () => {
   const { t } = useTranslation("common");
 
   return (
-    <>
-      <SectionTitle content={"about"} />
+    <div className="about">
+      <ScrollToTop />
+      <SectionTitleWithTranslation content={"about"}  />
+      <Row className="about__intro">
+        <Col sm={6}>
+          <h3>,,Jak przynieść światu<br />najwięcej dobra?”</h3>
+          <p>Efektywny Altruizm to międzynarodowa społeczność szukająca odpowiedzi na to pytanie. Zdobytą tą drogą wiedzę wykorzystujemy później w praktyce, pracując na rzecz obszarów, które najbardziej potrzebują naszej pomocy. Okazuje się, że przy użyciu ograniczonych środków możemy mieć wpływ na realne zmiany na świecie.</p>
+        </Col>
+        <Col sm={6}>
+        </Col>
+      </Row>
       <Row id="movement" className={`content-section justify-content-center`}>
         <Col xs={6} className="content-section__column mx-auto">
           <SectionTitle title={"about.movement"}></SectionTitle>
+          <Row>
+
+          </Row>
           <Paragraph content={"about.movement.intro"}></Paragraph>
           <div>
             {t("about.movement.intro", { returnObjects: true }).map((element, index) => (
@@ -45,7 +59,7 @@ const AboutPage = () => {
         </Col>
       </Row>
       <TeamSection />
-    </>
+    </div>
   );
 };
 
