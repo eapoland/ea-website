d5287345fc4bbf18dff8d84da70edcd952927d2b
Michał Hawełka <m.hawelka@gmail.com>
2021-02-21 17:04:42 +0100

    About page web

--- src/pages/About/AboutPage.js
+++ src/pages/About/AboutPage.js
@@ -10,7 +10,15 @@ import mtischner from "../../assets/images/team/mtischner.png";
 import mtrzesimiech from "../../assets/images/team/mtrzesimiech.png";
 import mjaryczewska from "../../assets/images/team/mjaryczewska.png";
 import mhawelka from "../../assets/images/team/mhawelka.png";
+import ksarek from "../../assets/images/team/consulting/ksarek.png";
+import dgogloza from "../../assets/images/team/consulting/dgogloza.png";
+import dwyrzykowski from "../../assets/images/team/consulting/dwyrzykowski.png";
+import mwozniak from "../../assets/images/team/consulting/mwozniak.png";
 import MemberCard from "./MemberCard";
+import ConsultantCard from "./ConsultantCard";
+import Slider from "react-slick";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 const teamMembers = [
   {
@@ -64,8 +72,51 @@ const teamMembers = [
 
 ]
 
+const consultants = [
+  {
+    name: "Karolina Sarek",
+    photo: ksarek,
+    desc: "Współzałożycielka i Dyrektor Badań w Charity Entrepreneurship Fund Manager at Effective Altruism Animal Welfare Fund",
+    email: "EMAIL",
+    linkedin: "URL"
+  },
+  {
+    name: "Dobrosława Gogłoza",
+    photo: dgogloza,
+    desc: "Współzałożycielka i Head of Strategy w All Hands Agency",
+    email: "EMAIL",
+    linkedin: "URL"
+  },
+  {
+    name: "Marcin Woźniak",
+    photo: mwozniak,
+    desc: "Współzałożyciel Fundacji Optimum Pareto i twórca Swarmcheck - deliberatywnego systemu wspomagania decyzji.",
+    email: "EMAIL",
+    linkedin: "URL"
+  },
+  {
+    name: "Daniel Wyrzykowski",
+    photo: dwyrzykowski,
+    desc: "Twórca Altruisto.com - aplikacji do zbiórki funduszy dla organizacji charytatywnych podczas zakupów online.",
+    email: "EMAIL",
+    linkedin: "URL"
+  }
+]
 
 const AboutPage = () => {
+  const settings = {
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    cssEase: "linear",
+    pauseOnHover: true,
+    className: "d-flex align-items-center",
+    dots: true,
+    prevArrow: <FontAwesomeIcon icon={faChevronLeft} className="ea-slider__arrow" />,
+    nextArrow: <FontAwesomeIcon icon={faChevronRight} className="ea-slider__arrow" />,
+  };
 
   return (
     <div className="about">
@@ -125,7 +176,49 @@ const AboutPage = () => {
               {teamMembers.map(member => (
                 <MemberCard key={member.name} teamMember={member} />
               ))}
+
             </div>
+        </Col>
+      </Row>
+      <Row className="about__team">
+        <Col>
+            <h3 className="text-center">Doradzają nam</h3>
+            <div className="d-flex flex-wrap justify-content-center about__team--consultants">
+              {consultants.map(member => (
+                <ConsultantCard key={member.name} consultant={member} />
+              ))}
+            </div>
+        </Col>
+      </Row>
+      <Row className="about__team">
+        <Col>
+            <h3 className="text-center">Pracowali z nami</h3>
+            <Slider {...settings} className="about__team--slider">
+              <div>
+                <div>
+                  <h5>Marcin Kowrygo</h5>
+                  <p>Neurobiolog, popularyzator otwartej i efektywnej nauki, główny strateg polskiej społeczności.</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <h5>Agnieszka Pisz</h5>
+                  <p>Projektantka usług i interakcji. Skupia się na rozwiązaniach, które polepszają jakość życia ludzi i ich otoczenia.</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <h5>Marcin Wosinek</h5>
+                  <p>Programista.</p>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <h5>Marcin Wosinek</h5>
+                  <p>Programista.</p>
+                </div>
+              </div>
+            </Slider>
         </Col>
       </Row>
     </div>
---
+++ src/pages/About/ConsultantCard.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Col from 'reactstrap/lib/Col'
+import Row from 'reactstrap/lib/Row'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+const ConsultantCard = ({consultant}) => {
+    return (
+        <div className="about__team--person">
+            <img src={consultant.photo} alt='' />
+            <div className="about__team--details">
+            <Row>
+                <Col sm={9}><h5>{consultant.name}</h5></Col>
+                <Col sm={3} className="d-flex">
+                    <a href={consultant.linkedin} target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faLinkedinIn} className="social-icon" />
+                    </a>
+                    <a
+                    href={`mailto:${consultant.email}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                    <FontAwesomeIcon icon={faEnvelope} className="social-icon" />
+                    </a>
+                </Col>
+            </Row>
+            <p>{consultant.desc}</p>
+            </div>
+        </div>
+    )
+}
+
+export default ConsultantCard
