057d499917793861fb7bf48a58e4dcd9ada4894e
Michał Hawełka <m.hawelka@gmail.com>
2021-02-23 20:07:01 +0100

    Comp fix

--- src/index.js
+++ src/index.js
@@ -6,17 +6,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./index.scss";
-import { Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
 
 ReactGA.initialize("UA-79994527-1");
-const history = createBrowserHistory();
 
-history.listen(entry => {
-  ReactGA.set({ page: entry.location.pathname }); // Update the user's current page
-  ReactGA.pageview(entry.location.pathname); // Record a pageview for the given page
-});
 
 ReactGA.event({
   category: "Test event",
@@ -25,9 +19,9 @@ ReactGA.event({
 
 ReactDOM.render(
   <React.StrictMode>
-      <Router history={history}>
+      <BrowserRouter>
         <App />
-      </Router>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
