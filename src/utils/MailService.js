68a46a3c72e37eb6e00d591170e1a5bea0a68cae
Michał Hawełka <m.hawelka@gmail.com>
2021-03-03 06:06:46 +0100

    Test

--- src/utils/MailService.js
+++ src/utils/MailService.js
@@ -3,21 +3,21 @@ import axios from "axios";
 const MailService = {
     sendMail(name, email, subject, msg) {
         axios.post("https://ea-website-mail.azurewebsites.net/api/mail?code=ujJvutExsTSUPGf30prVxT7UVOVZ4OMn38EheQO8jnhRdN5OfWY8xQ==", {
-            sender: {
-                name: "Michał Trzęsimiech",
-                email: "michal.trzesimiech@efektywnyaltruizm.org" 
+            "sender": {
+                "name": "Michał Trzęsimiech",
+                "email": "michal.trzesimiech@efektywnyaltruizm.org" 
             },
-            recipients: [
+            "recipients": [
                 {
-                    name: name,
-                    email: email
+                    "name": name,
+                    "email": email
                 },
             ],
-            templateId: "d-0ebdd476938a42548085491228209eb6",
-            templateData: {
-                formType: "Test form",
-                subject: subject,
-                content: msg
+            "templateId": "d-0ebdd476938a42548085491228209eb6",
+            "templateData": {
+                "formType": "Test form",
+                "subject": subject,
+                "content": msg
             }
         })
     }
